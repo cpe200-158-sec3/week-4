fa1b46e43104245a4f0b17ccc14a4df0d387c0d6
pawiseiei <pawis.jp@hotmail.com>
2015-09-11 21:50:06 +0700

    addall

---
+++ lab401/Circle.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab401
+{
+    class Circle : Point
+    {
+        protected double radius;
+        protected Point center;
+
+
+        public double Radius
+        {
+            get { return radius; }
+            set
+            {
+                if (value >= 0) { radius = value; }
+                else { Console.WriteLine("Error Radius must be at least 0"); }
+            }
+        }
+
+        public Point Center
+        {
+            get { return center; }
+        }
+
+        public Circle()
+            : base(0, 0)
+        {
+            radius = 1.0;
+
+        }
+
+        public Circle(double rad)
+            : base(0, 0)
+        {
+            radius = rad;
+
+        }
+
+        public Circle(double x, double y, double rad)
+            : base(x, y)
+        {
+            radius = rad;
+
+        }
+
+        public Circle(Circle c)
+            : base(c.X, c.Y)
+        {
+            radius = c.Radius;
+
+        }
+
+        public void setCircle(double x, double y, double rad)
+        {
+            base.X = x;
+            base.Y = y;
+            radius = rad;
+
+        }
+        public virtual double getArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("[Circle: center({0},{1}), radius={2}, area={3}]", base.X, base.Y, Radius, getArea());
+        }
+    }
+}
---
+++ lab401/Cylinder.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab401
+{
+    class Cylinder : Circle
+    {
+        protected double height;
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value >= 0) { height = value; }
+                else { Console.WriteLine("Error Height must be at least 0"); }
+            }
+        }
+
+        public Cylinder()
+            : base(0, 0, 1)
+        {
+            height = 1;
+        }
+
+        public Cylinder(double rad, double height)
+            : base(0, 0, rad)
+        {
+            this.height = height;
+        }
+
+        public Cylinder(double x, double y, double rad, double height)
+            : base(x, y, rad)
+        {
+            this.height = height;
+        }
+
+        public Cylinder(Circle c)
+            : base(c)
+        {
+            height = 1;
+        }
+        public Cylinder(Cylinder c)
+            : base(c.X, c.Y, c.Radius)
+        {
+            height = c.Height;
+
+        }
+        public override double getArea()
+        {
+            return base.getArea() * 2 + 2 * Math.PI * base.Radius * Height;
+        }
+
+        public double getVolume()
+        {
+            return base.getArea() * Height;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[Cylinder: center({0},{1}), radius={2}, height={3}, surface={4}, volume={5}]", base.X, base.Y, base.Radius, Height, getArea(), getVolume());
+        }
+    }
+}
