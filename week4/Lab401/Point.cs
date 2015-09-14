f83fe83ddbc93b119a8c2f7600ad04aef20d5e1f
normandysr1 <flukery@gmail.com>
2015-09-14 23:53:33 +0700

    (Week4 lab401) Point.cs

--- week4/Lab401/Point.cs
+++ week4/Lab401/Point.cs
@@ -1,12 +1,54 @@
-﻿using System;
+using System;
 
-namespace Lab4
+namespace Lab401
 {
-	public class Point
-	{
-		public Point ()
-		{
-		}
-	}
+    class Point
+    {
+        protected double x;
+        protected double y;
+
+        public double X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+        public double Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+        public Point()
+        {
+            x = 0;
+            y = 0;
+        }
+        public Point(Point a)
+        {
+            x = a.x;
+            y = a.y;
+        }
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public override string ToString()
+        {
+            return "center (" + this.x + ", " + this.y + ")";
+        }
+    }
 }
+
 
