275bfd2bef52c1c003cd85556167b87c71a8611a
Tthungmay <tthungmay@hotmail.com>
2015-09-15 16:56:26 +0700

    --- shape class done ---

--- week4/Lab402/Shape.cs
+++ week4/Lab402/Shape.cs
@@ -4,9 +4,25 @@ namespace Lab4
 {
 	public abstract class Shape
 	{
-		public Shape ()
+        private readonly string Color = "gold";
+
+        public string color
+        {
+            get { return Color; }
+        }
+
+		public Shape (string c)
 		{
+            Color = c;
 		}
+
+        public abstract double getArea();
+        public abstract double getPerimeter();
+
+        public override string ToString()
+        {
+            return string.Format(Color);
+        }
 	}
 }
 
