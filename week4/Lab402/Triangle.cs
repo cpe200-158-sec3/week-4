a049c4565b2e81bbabb0d1a1914505b94903ae1a
napatsawan <lfc_vl2009@hotmail.com>
2015-09-16 00:09:34 +0700

    Triangle.cs complete

--- week4/Lab402/Triangle.cs
+++ week4/Lab402/Triangle.cs
@@ -53,6 +53,7 @@ namespace Lab4
         {
             return Height + Math.Sqrt(Math.Pow(Height, 2) + Math.Pow(Width, 2)) + Width;
         }
+
         public override string ToString()
         {
             return string.Format("[Triangle: base={0}, height={1}, area={2}, color={3}]", Width, Height, getArea(), Color);
