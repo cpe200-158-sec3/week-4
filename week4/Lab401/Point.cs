8bfba9f4fa3629dee4b556d68ee674df0c283130
STNMPTW <theonegame.1@gmail.com>
2015-09-15 23:24:56 +0700

    complete

--- week4/Lab401/Point.cs
+++ week4/Lab401/Point.cs
@@ -2,11 +2,30 @@
 
 namespace Lab4
 {
-	public class Point
-	{
-		public Point ()
-		{
-		}
-	}
+    public class Point
+    {
+        private double x;
+        private double y;
+        public double X { set { x = value; } get { return x; } }
+        public double Y { set { y = value; } get { return y; } }
+        public Point()
+        {
+            X = 0;
+            Y = 0;
+        }
+        public Point(Point a)
+        {
+            X = a.X;
+            Y = a.Y;
+        }
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public override string ToString()
+        {
+            return string.Format("center({0},{1})",X,Y);
+        }
+    }
 }
-
