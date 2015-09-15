66cd99cd4ba569d1cd91ec4baa52fa0694938e40
toptenshadow <topten_non-prince@hotmail.com>
2015-09-15 15:08:18 +0700

    Circle.cs

---
+++ lab401/lab401/Circle.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab401
+{
+    class Circle : Point
+    {
+        protected Point center;
+        protected double radius;
+
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                if (radius >= 0)
+                {
+                    radius = value;
+                }
+                else
+                {
+                    Console.WriteLine("invalid radius");
+                }
+            }
+        }
+        public Point Center
+        {
+            get
+            {
+                return center;
+            }
+        }
+
+        public Circle()
+            : base(0, 0)
+        {
+            radius = 1;
+        }
+        public Circle(double r)
+            : base(0, 0)
+        {
+            radius = r;
+        }
+        public Circle(double x, double y, double r)
+            : base(x, y)
+        {
+            radius = r;
+        }
+        public Circle(Circle c)
+            : base(c.x, c.y)
+        {
+            radius = c.radius;
+        }
+        public void setCircle(double x, double y, double r)
+        {
+            base.X = x;
+            base.Y = y;
+            radius = r;
+        }
+        public virtual double getArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+        public override string ToString()
+        {
+            return "[Circle: center(" + base.X + "," + base.Y + "), " + "Radius=" + radius + ", Area=" + getArea() + "]";
+        }
+    }
+}
