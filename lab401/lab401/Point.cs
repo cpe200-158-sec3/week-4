9079643fc0b5efd568583d21c30c2060676dadc1
toptenshadow <topten_non-prince@hotmail.com>
2015-09-15 15:09:08 +0700

    Point.cs

---
+++ lab401/lab401/Point.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab401
+{
+    class Point
+    {
+         protected double x,y;
+
+        public double X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+        public double Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+        public Point()
+        {
+           this.x = 0;
+            this.y = 0;
+        }
+        public Point(Point a)
+        {
+            x = a.x;
+            y = a.y;
+        }
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public override string ToString()
+        {
+            return "center (" + this.x + ", " + this.y + ")";
+        }
+    }
+}
