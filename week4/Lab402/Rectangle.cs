a83516e688b646a53d25e5c476283921f6a3394a
tuinuisureerat <tuinui-sureerat@hotmail.com>
2015-09-15 20:26:34 +0700

    create program Rectangle Shape Triangle

--- week4/Lab402/Rectangle.cs
+++ week4/Lab402/Rectangle.cs
@@ -2,11 +2,79 @@
 
 namespace Lab4
 {
-	public class Rectangle
-	{
-		public Rectangle ()
-		{
-		}
-	}
-}
+        public class Rectangle : Shape
+        {
+            private double width;
+            private double height;
+
+            public double Width
+            {
+                get
+                {
+                    return width;
+                }
+                set
+                {
+                    if (value > 0)
+                    {
+                        width = value;
+                    }
+                    else
+                    {
+                        width = 1;
+                    }
+                }
+            }
+
+            public double Height
+            {
+                get
+                {
+                    return height;
+                }
+                set
+                {
+                    if (value > 0)
+                    {
+                        height = value;
+                    }
+                    else
+                    {
+                        height = 1;
+                    }
+                }
+            }
+            public Rectangle() : base("gold")
+            {
+                width = 1;
+                height = 1;
+            }
+            public Rectangle(string color, double w, double h): base(color)
+            {
+                 width = w;
+                height = h;
+            }
+            public Rectangle(Rectangle r) : base(r.Color)
+            {
+                width = r.width;
+                height = r.height;
+            }
+            public override double getArea()
+            {
+                return width * height;
+            }
+
+            public override double getPerimeter()
+            {
+                return 2 * (Height + Width);
+            }
+            public override string ToString()
+            {
+                return string.Format("[Rectangle: width={0}, height={1}, area={2}, color={3}]", width, height, getArea(), Color);
+            }
+        }
+    }
+
+
+
 
--- week4/Lab402/Shape.cs
+++ week4/Lab402/Shape.cs
@@ -2,11 +2,33 @@
 
 namespace Lab4
 {
-	public abstract class Shape
-	{
-		public Shape ()
-		{
-		}
-	}
-}
+
+        public abstract class Shape
+        {
+            private readonly string _Color = "gold";
+            public string Color
+            {
+                get
+                {
+                    return _Color;
+                }
+            }
+            public Shape(string color)
+            {
+                _Color = color;
+            }
+
+            public abstract double getArea();
+            public abstract double getPerimeter();
+
+            public override string ToString()
+            {
+                return "";
+            }
+
+        }
+    }
+
+
+
 
