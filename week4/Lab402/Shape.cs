d0ced66b7e20ea2c0f6d850b485d4c6b236e0512
napatsawan <lfc_vl2009@hotmail.com>
2015-09-16 00:08:19 +0700

    Shape.cs complete

--- week4/Lab402/Shape.cs
+++ week4/Lab402/Shape.cs
@@ -2,11 +2,23 @@
 
 namespace Lab4
 {
-	public abstract class Shape
-	{
-		public Shape ()
-		{
-		}
-	}
+    public abstract class Shape
+    {
+        private readonly string _color = "gold";
+        public string Color
+        {
+            get { return _color; }
+        }
+        public Shape(string color)
+        {
+            _color = color;
+        }
+        public abstract double getArea();
+        public abstract double getPerimeter();
+        public override string ToString()
+        {
+            return string.Format(_color);
+        }
+    }
 }
 
