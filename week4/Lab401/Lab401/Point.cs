5233231ded8447daeb46457c7a071cfb45f3ecc8
ze.mandkarn@gmail.com <ze.mandkarn@gmail.com>
2015-09-15 15:12:09 +0700

    finish

---
+++ week4/Lab401/Lab401/Point.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public class Point
+    {
+        public double x, y;
+        public Point()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Point(Point p)
+        {
+            this.x = p.x;
+            this.y = p.y;
+        }
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public override string ToString()
+        {
+            string p;
+            p = "center(" + x + "," + y + ")";
+            return p;
+        }
+
+
+    }
+}
+
