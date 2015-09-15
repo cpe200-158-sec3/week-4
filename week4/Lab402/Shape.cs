52bc01ccb3aefa97fbf7c5f3c8634c92b1e9549e
Nrtdemo <nrtdemo@hotmail.com>
2015-09-15 15:01:14 +0700

    add file lab401,lab402 and lab403.

--- week4/Lab402/Shape.cs
+++ week4/Lab402/Shape.cs
@@ -4,9 +4,22 @@ namespace Lab4
 {
 	public abstract class Shape
 	{
-		public Shape ()
+        private readonly string _Color = "gold";
+        public string color
+        {
+            get { return _Color; }
+        }
+        
+		public Shape(string c)
 		{
+            _Color = c;
 		}
-	}
+        public abstract double getArea();
+        public abstract double getPerimeter();
+        public override string ToString()
+        {
+            return string.Format(_Color);
+        }
+    }
 }
 
