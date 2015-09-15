57ec956431c83a997aa96076ae4a739d69b04f47
Taksaporn <chaikeawmay.t@gmail.com>
2015-09-15 23:46:15 +0700

    Add shape class ,rectangle class, triangle class and answer question readme

---
+++ week4/Lab402/Lab402/Lab402/Rectangle.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Lab402
+{
+	public class Rectangle:Shape
+	{
+        private double width;
+        private double height;
+
+        public double Width {
+            get { return width; }
+            set { if (value > 0)
+                    {
+                        width = value;
+                    }
+                    else {
+                        width = 1.0;
+                    }
+                }
+        }
+
+        public double Height {
+            get { return height; }
+            set
+            {
+                if (value > 0)
+                {
+                    height = value;
+                }
+                else
+                {
+                    height = 1.0;
+                }
+            }
+        }
+
+		public Rectangle ():base("gold")
+		{
+            height = 1.0;
+            width = 1.0;
+		}
+
+        public Rectangle(string color, double width, double height):base(color) {
+            //this.Color = color;
+            this.width = width;
+            this.height = height;
+        }
+
+        public Rectangle(Rectangle rectangle):base(rectangle.Color) {
+            //this.Color = rectangle.Color;
+            this.width = rectangle.width;
+            this.height = rectangle.height;
+        }
+
+        public override double getArea() {
+            return width * height;
+        }
+
+
+        public override double getPerimeter()
+        {
+            return (2 * (height + width));
+        }
+
+        //[Rectangle: width=4, height=5, area=20, color="red"]
+        public override string ToString()
+        {
+            return "[Rectangle: width="+width+", height="+height+", area="+getArea()+", color="+Color+"]";
+        }
+    }
+}
+
