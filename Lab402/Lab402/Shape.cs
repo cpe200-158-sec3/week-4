19006d81d7e020b366054ce23a41e4c0eb6de6b7
princejomzas <princejomza@gmail.com>
2015-09-15 20:44:44 +0700

    Shape class (Modify)

--- Lab402/Lab402/Shape.cs
+++ Lab402/Lab402/Shape.cs
@@ -4,6 +4,44 @@ namespace Lab4
 {
 	public abstract class Shape
     {
+        protected double width;
+        protected double height;
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    width = value;
+                }
+                else
+                {
+                    width = 1;
+                }
+            }
+        }
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    height = value;
+                }
+                else
+                {
+                    height = 1;
+                }
+            }
+        }
         protected readonly string color = "gold";
         public Shape(string c)
         {
