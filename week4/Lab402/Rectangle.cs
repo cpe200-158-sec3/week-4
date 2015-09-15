d631bbe63ee4f310e6b7ed5d3a6fc18e311adb7d
pawiseiei <pawis.jp@hotmail.com>
2015-09-15 23:13:27 +0700

    resend lab401 and send lab402

--- week4/Lab402/Rectangle.cs
+++ week4/Lab402/Rectangle.cs
@@ -1,12 +1,70 @@
 ﻿using System;
 
-namespace Lab4
+namespace Lab402
 {
-	public class Rectangle
-	{
-		public Rectangle ()
-		{
-		}
-	}
+    public class Rectangle : Shape
+    {
+        private double _Width, _Height;
+        public double Width
+        {
+            get { return _Width; }
+            set
+            {
+                if (value > 0)
+                {
+                    _Width = value;
+                }
+                else
+                {
+                    _Width = 1.0;
+                }
+            }
+        }
+        public double Height
+        {
+            get { return _Height; }
+            set
+            {
+                if (value > 0)
+                {
+                    _Height = value;
+                }
+                else
+                {
+                    _Height = 1;
+                }
+            }
+        }
+        public Rectangle()
+            : base("gold")
+        {
+            _Height = 1;
+            _Width = 1;
+        }
+        public Rectangle(string c, double w, double h)
+            : base(c)
+        {
+            _Height = h;
+            _Width = w;
+        }
+        public Rectangle(Rectangle r)
+            : base(r.color)
+        {
+            _Height = r._Height;
+            _Width = r._Width;
+        }
+        public override double getArea()
+        {
+            return _Height * _Width;
+        }
+        public override double getPerimeter()
+        {
+            return 2 * (_Height + _Width);
+        }
+        public override string ToString()
+        {
+            return string.Format("[Rectangle: width={0}, height={1}, area={2}, color={3}]", _Width, _Height, getArea(), color);
+        }
+    }
 }
 
