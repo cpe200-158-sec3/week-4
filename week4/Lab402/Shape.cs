af8eb4c4cef987494575692a0155e9678bd29060
toptenshadow <topten_non-prince@hotmail.com>
2015-09-15 21:33:22 +0700

    Create Shape class

--- week4/Lab402/Shape.cs
+++ week4/Lab402/Shape.cs
@@ -4,9 +4,22 @@ namespace Lab4
 {
 	public abstract class Shape
 	{
-		public Shape ()
+        private readonly string _color = "gold";
+        public string Color{
+            get {
+                return _color;
+            }
+        }
+		public Shape (string c)
 		{
+            _color = c;
 		}
+        public abstract double getArea();
+        public abstract double getPerimeter();
+        public override string ToString()
+        {
+         return string.Format(_color);
+        }
 	}
 }
 
