effab04611dfb4a01fd39c1d224312311f81f65a
vivit-phruktayanon <vivitphruktayanon@gmail.com>
2015-09-15 23:12:55 +0700

    commit Point

---
+++ week4/Week4/Point.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lab4
+{
+	public class Point
+
+
+	{
+        private double _x;
+        private double _y;
+        public double X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+        public double Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+        public Point()
+        {
+            X = 0;
+            Y = 0;
+        }
+        public Point(Point a)
+        {
+            X = a.X;
+            Y = a.Y;
+
+        }
+        public Point(double a, double b)
+        {
+            X = a;
+            Y = b;
+        }
+        public override string ToString()
+        {
+            return String.Format("");
+        }
+	}
+}
+
