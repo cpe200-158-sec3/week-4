dfe6802d6c31228cfba67f34e40ad73644e67c8d
suphaloet <suphaloet.skp@gmail.com>
2015-09-15 22:48:30 +0700

    401 complete

--- week4/Lab401/Cylinder.cs
+++ week4/Lab401/Cylinder.cs
@@ -2,11 +2,58 @@
 
 namespace Lab4
 {
-	public class Cylinder
-	{
-		public Cylinder ()
-		{
-		}
-	}
+    public class Cylinder : Circle
+    {
+        private double _height = 1;
+        private double Height
+        {
+            get { return _height; }
+            set
+            {
+                if (value >= 0)
+                {
+                    _height = value;
+                }
+            }
+        }
+        public Cylinder()
+        {
+            this.Height = 1;
+        }
+        public Cylinder(double radius, double height) : base(radius)
+        {
+            this.Height = height;
+        }
+        public Cylinder(double x, double y, double radius, double height) : base(x, y, radius)
+        {
+            this.Height = height;
+        }
+        public Cylinder(Circle c) : base(c)
+        {
+            this.Height = 1;
+        }
+        public Cylinder(Cylinder Cy) : base(Cy.x, Cy.y, Cy.getRadius())
+        {
+            this.Height = Cy.Height;
+        }
+        public double getArea()
+        {
+            double area;
+            area = (2 * Math.PI * getRadius() * Height) + (2 * Math.PI * Math.Pow(getRadius(), 2));
+            return area;
+        }
+        public double getVolume()
+        {
+            double volume;
+            volume = (Math.PI * Math.Pow(getRadius(), 2)) * Height;
+            return volume;
+        }
+        public override string ToString()
+        {
+            string s;
+            s = "[Cylinder: center(" + x + "," + y + "), radius=" + getRadius() + ", height=" + Height + ", surface=" + getArea() + ", volume=" + getVolume() + "]";
+            return s;
+        }
+    }
 }
 
--- week4/Lab401/Point.cs
+++ week4/Lab401/Point.cs
@@ -2,11 +2,32 @@
 
 namespace Lab4
 {
-	public class Point
-	{
-		public Point ()
-		{
-		}
-	}
+    public class Point
+    {
+        public double x, y;
+        public Point()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Point(Point p)
+        {
+            this.x = p.x;
+            this.y = p.y;
+        }
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public override string ToString()
+        {
+            string s;
+            s = "center(" + x + "," + y + ")";
+            return s;
+        }
+    }
 }
 
