c8a91af2da68399283cd524a3db8a1e4d01c0155
Jiraphat <lesaku--like@hotmail.com>
2015-09-16 16:11:24 +0700

    finish lab402

---
+++ Lab402/Lab402/Shape.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab402
+{
+    public abstract class Shape
+    {
+        private readonly string _Color = "gold";
+        public string color
+        {
+            get
+            {
+                return _Color;
+            }
+        }
+
+        public Shape(string c)
+        {
+            _Color = c;
+        }
+        public abstract double getArea();
+        public abstract double getPerimeter();
+        public override string ToString()
+        {
+            return string.Format(_Color);
+        }
+    }
+}
