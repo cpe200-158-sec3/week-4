2ec8b8131e3253cf5632649494929666ed29ddc1
nahataichanok rojcharoenkul <alone_l3@hotmail.com>
2015-11-10 15:42:33 +0700

    401 again

---
+++ week4/Lab401/Lab401/Lab401/Point.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Lab401
+{
+	public class Point
+	{
+		protected double x;
+		protected double y;
+
+		public double X
+		{
+			get 
+			{
+				return x;
+			}
+			set 
+			{
+				x = value;
+			}
+		}
+		public double Y
+		{
+			get 
+			{
+				return y;
+			}
+			set 
+			{
+				y = value;
+			}
+		}
+
+		public Point ()
+		{
+			x = 0;
+			y = 0;
+		}
+		public Point (Point a)
+		{
+			x = a.X;
+			y = a.Y;
+		}
+		public Point (double a, double b)
+		{
+			x = a;
+			y = b;
+		}
+		public override string ToString ()
+		{
+			return string.Format ("[Point: X={0}, Y={1}]", X, Y);
+		}
+	}
+}
+
+//circle
+//testcommit
+
+//commit
